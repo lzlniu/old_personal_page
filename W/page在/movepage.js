445881ea81821c12d2f890349617fe835cf5dafc
lzlniu <lzlniu@qq.com>
2019-11-24 19:14:29 +0800

    changing content and main page

--- W/page在/movepage.js
+++
@@ -1,9 +0,0 @@
-//<script type="text/javascript">
-$(document).ready(function() {
-	$('a').click(function(e) {
-		e.preventDefault();
-		$('#p1').toggleClass('move');
-		$('#p2').toggleClass('move');
-	});
-});
-//</script>
